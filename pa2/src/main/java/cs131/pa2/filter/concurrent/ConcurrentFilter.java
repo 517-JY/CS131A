012566ac245739287b43059b12e1bab1f6d18416
Jiayin Li <jiayin.li007@outlook.com>
2021-04-06 20:54:07 -0400

    Multi-thread partially update

--- pa2/src/main/java/cs131/pa2/filter/concurrent/ConcurrentFilter.java
+++ pa2/src/main/java/cs131/pa2/filter/concurrent/ConcurrentFilter.java
@@ -1,25 +1,32 @@
 package cs131.pa2.filter.concurrent;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 import cs131.pa2.filter.Filter;
 
 /**
  * An abstract class that extends the Filter and implements the basic functionality of all filters. Each filter should
  * extend this class and implement functionality that is specific for that filter.
- * @author cs131a
+ * In addition, the class will implement the Runnable interface to allow
+ *      1. different filters to be started as separate threads
+ *      2. different filters to run concurrently
+ * @author cs131a, Jiayin Li
  *
  */
-public abstract class ConcurrentFilter extends Filter {
-	/**
-	 * The input queue for this filter
-	 */
-	protected Queue<String> input;
-	/**
-	 * The output queue for this filter
-	 */
-	protected Queue<String> output;
+public abstract class ConcurrentFilter extends Filter implements Runnable{
 	
+	/**
+	 * The input LinkedBlockingQueue for this filter
+	 * Reason why choosing LinkedBlockingQueue
+	 * 		1. using internal locks to keep thread-safe 
+	 *      2. only conducts add(put()) and remove(take()) implementations when it makes sense
+	 */
+	protected LinkedBlockingQueue<String> input = new LinkedBlockingQueue<String>();
+	/**
+	 * The output LinkedBlockingQueue for this filter
+	 */
+	protected LinkedBlockingQueue<String> output= new LinkedBlockingQueue<String>();
+	
+		
 	@Override
 	public void setPrevFilter(Filter prevFilter) {
 		prevFilter.setNextFilter(this);
@@ -32,13 +39,14 @@ public abstract class ConcurrentFilter extends Filter {
 			this.next = concurrentNext;
 			concurrentNext.prev = this;
 			if (this.output == null){
-				this.output = new LinkedList<String>();
+				this.output = new LinkedBlockingQueue<String>();
 			}
 			concurrentNext.input = this.output;
 		} else {
 			throw new RuntimeException("Should not attempt to link dissimilar filter types.");
 		}
 	}
+	
 	/**
 	 * Gets the next filter
 	 * @return the next filter
@@ -46,23 +54,55 @@ public abstract class ConcurrentFilter extends Filter {
 	public Filter getNext() {
 		return next;
 	}
+	
+	
 	/**
-	 * processes the input queue and writes the result to the output queue
+	 * Lets the sequence of filters run concurrently  
+	 * Creates separate threads for each filter respectively
 	 */
-	public void process(){
+	public void process(){		
 		while (!input.isEmpty()){
-			String line = input.poll();
-			String processedLine = processLine(line);
-			if (processedLine != null){
-				output.add(processedLine);
+			String line;
+			try {
+				// Uses API method for BlockingQueue
+				// Only takes line when there are available items to remove 
+				line = input.take();
+				String processedLine = processLine(line);
+				if (processedLine != null){
+					// Only puts item into output when the LinkedBlockingQueue is not full
+					output.put(processedLine);
+				}
+			} catch (InterruptedException e1) {
+				e1.printStackTrace();
 			}
 		}	
 	}
 	
+	
+	/**
+	 * Checks whether those filters have finished their jobs
+	 * Mainly use within the printFilter and RedirectFilter
+	 * Current filter is only marked as finished if the previous filter is finished
+	 */
 	@Override
-	public boolean isDone() {
-		return input.size() == 0;
+	public boolean isDone() {		
+		if (prev != null) {
+			return prev.isDone() && input.isEmpty();
+		}		
+		return input.isEmpty();	
 	}
+	
+
+	/**
+	 * The method that inherits from the Runnable interface
+	 * Calls the process() method which let the sequence of filters run concurrently
+	 */	
+	public void run() {
+//		// Prints running thread information for sub-phases checking
+//		System.out.println(Thread.currentThread().toString() + " is running ....");
+		process();		
+	}	
+	
 	
 	protected abstract String processLine(String line);
 	
--- pa2/src/main/java/cs131/pa2/filter/concurrent/ConcurrentREPL.java
+++ pa2/src/main/java/cs131/pa2/filter/concurrent/ConcurrentREPL.java
@@ -1,5 +1,7 @@
 package cs131.pa2.filter.concurrent;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 import cs131.pa2.filter.Message;
@@ -7,7 +9,7 @@ import cs131.pa2.filter.Message;
 /**
  * The main implementation of the REPL loop (read-eval-print loop).
  * It reads commands from the user, parses them, executes them and displays the result.
- * @author cs131a
+ * @author cs131a, Jiayin Li
  *
  */
 public class ConcurrentREPL {
@@ -15,6 +17,8 @@ public class ConcurrentREPL {
 	 * the path of the current working directory
 	 */
 	static String currentWorkingDirectory;
+	
+	
 	/**
 	 * The main method that will execute the REPL loop
 	 * @param args not used
@@ -24,6 +28,9 @@ public class ConcurrentREPL {
 		Scanner s = new Scanner(System.in);
 		System.out.print(Message.WELCOME);
 		String command;
+		
+		Queue<ConcurrentFilter> filters = new LinkedList<ConcurrentFilter>();
+		
 		while(true) {
 			//obtaining the command from the user
 			System.out.print(Message.NEWCOMMAND);
@@ -31,14 +38,42 @@ public class ConcurrentREPL {
 			if(command.equals("exit")) {
 				break;
 			} else if(!command.trim().equals("")) {
-				//building the filters list from the command
+				//Builds the filters list from the command
 				ConcurrentFilter filterlist = ConcurrentCommandBuilder.createFiltersFromCommand(command);
+				
+				//Collects all filters before starting any of them 
 				while(filterlist != null) {
-					filterlist.process();
+					// Adds all valid filters into a Queue container
+					filters.add(filterlist);
 					filterlist = (ConcurrentFilter) filterlist.getNext();
 				}
+				
+								
+				while(!filters.isEmpty())
+				{
+					// Generates thread that follows the FIFO principle
+					ConcurrentFilter filter =  filters.poll();
+					Thread t = new Thread(filter);
+					
+					try {
+						// Makes a short break here to ensure that filter (sub-command) comes late also starts late
+						Thread.sleep(100);
+					} catch (InterruptedException e1) {
+						e1.printStackTrace();
+					}
+	                
+					t.start();
+					
+					try {
+						// Waits for all threads to finish before going to the next command request
+						t.join();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}							
+				}							
 			}
 		}
+		
 		s.close();
 		System.out.print(Message.GOODBYE);
 	}
